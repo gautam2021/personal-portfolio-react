8f7b432b3fcd1b6e3aa7bdd7587ff542d424d24a
Mohit Kumar Gautam <mohitrecson1610@gmail.com>
2023-12-22 18:47:58 +0530

    Update About.jsx
    Added on 22-Dec-2023 : Whole journey- About

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -28,7 +28,7 @@ const About = () => {
             <div className="about__info">
               <h1>Hi There</h1>
               <div className="about__info-p1">
-               My name is Mohit Kumar Gautam and I am currently working as a Software QA Engineer at SS&C Technologies for 2 years. alongwith I had the opportunity to gain full-stack web development Trainee experience for 7 months at Newton School.
+               My name is Mohit Kumar Gautam and currently working as a Software QA Engineer at SS&C Technologies for 2.2 years. Along with I had the opportunity to gain Full Stack Web Development Trainee experience for 7 months at Newton School.
               </div>
               <div className="about__info-p2">
               working regularly to update myself with the new era of Computer Technologies
