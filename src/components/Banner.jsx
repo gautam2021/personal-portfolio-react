94f578291f8938f0740e7fd8df91c39381d5ed9b
Mohit Kumar Gautam <mohitrecson1610@gmail.com>
2024-03-17 14:23:41 +0530

    Update Banner.jsx
    added intro

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -10,7 +10,7 @@ const Banner = () => {
   const [state] = React.useState({
     title: "Mohit Kumar Gautam",
     text:
-      "A passionate Software QA Engineer at SS&C Technologies. I have a great understanding of testing tools, automation frameworks, and methodologies, excel in designing, scripting. Expert in Functional Testing, Integration testing, Regression testing as well as Performance testing, API testing and much more."
+      "A passionate Software QA Engineer at SS&C Technologies. Specialize in performing detailed analysis on user stories for critical projects. Expert in testing tools, automation frameworks, and their methodologies. Great understanding of selenium, functional, integration and regression testing, API testing, and much more."
   });
   return (
     <header className="header">
